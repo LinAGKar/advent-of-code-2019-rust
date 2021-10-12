1f8416b2c09b1cf4906ee82fcdad5d03f78179a7
Linus Kardell <linus.kardell@gmail.com>
2021-10-12 20:48:58 +0200

    [day5a] Only print last output

--- day5a/src/main.rs
+++ day5a/src/main.rs
@@ -3,10 +3,14 @@ fn main() {
     std::io::stdin().read_line(&mut input).unwrap();
     let mut computer = intcode::IntCode::new(input.trim().split(',').map(|x| x.parse().unwrap()).collect());
     computer.put_input(1);
+    let mut prev_output = 0;
 
     while computer.iterate() {
         while let Some(output) = computer.get_output() {
-            println!("{}", output);
+            assert_eq!(prev_output, 0);
+            prev_output = output;
         }
     }
+
+    println!("{}", prev_output);
 }
